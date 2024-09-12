1f977ea765aaf748dc71bb8072a8a9a07ff0ffb7
yousefn <jon772002@gmail.com>
2024-09-12 19:19:05 +0300

    t

--- src/components/home/services/Services.jsx
+++ src/components/home/services/Services.jsx
@@ -3,13 +3,13 @@ import React from "react";
 import Service from "./service/Service";
 
 const Services = () => {
-  const linkStyles = "p-2 bg-white rounded block";
+  const linkStyles = "font-medium p-2 bg-white rounded block hover:bg-[#048D5A] hover:text-white transition-all"; 
   return (
     <div className="bg-bgGray p-4 flex flex-col gap-2 h-full rounded-2xl">
       <div className="flex justify-between">
         <h3>أبرز الخدمات</h3>
         <nav className="flex gap-4">
-          <Link className={linkStyles} href={"/"}>
+          <Link className={`${linkStyles} !bg-[#048D5A] !text-white`} href={"/"}>
             الاكثر استخداما
           </Link>
           <Link className={linkStyles} href={"/"}>
