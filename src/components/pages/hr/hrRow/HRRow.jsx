b580a2615ba998ace3a09806df79cd246aa4b994
yousefn <jon772002@gmail.com>
2024-09-19 01:56:24 +0300

    t

--- src/components/pages/hr/hrRow/HRRow.jsx
+++ src/components/pages/hr/hrRow/HRRow.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
-const HRRow = ({ data: { id, name, title, department, status } }) => {
+const HRRow = ({
+  data: { id, name, title, department, status, vacations, work, loans },
+}) => {
+  const [state, setState] = useState(false);
   // console.log(data)
   // const cellStyles =;
   //bg-[#FAF9F4]
@@ -9,7 +12,7 @@ const HRRow = ({ data: { id, name, title, department, status } }) => {
       <div
         className={`bg-bgGreen flex gap-2 p-4 items-center HR-row rounded-lg relative`}
       >
-        <div className={`min-w-[100px] max-w-[20%] `}>
+        <div className={`w-[10%]`}>
           <p className="text-base ">{id}</p>
         </div>
         <div className="row-data-content">
@@ -24,6 +27,73 @@ const HRRow = ({ data: { id, name, title, department, status } }) => {
         <div className="row-data-content">
           <p className="">{status}</p>
         </div>
+        <div className="w-[10%]">
+          <button
+            className="bg-[#048D5A] text-white px-4 py-2 rounded text-sm"
+            onClick={() => setState(!state)}
+          >
+            التفاصيل
+          </button>
+        </div>
+      </div>
+      <div
+        className={`${
+          state ? "p-4" : "overflow-hidden max-h-0"
+        } flex gap-4 HR-row-details transition-all`}
+      >
+        <section>
+          <h3>الأجازات</h3>
+          {/* #F1F0F8 */}
+          <div
+            className={`bg-[#F1F0F8] after:!bg-[#9586f5] flex gap-2 p-4 items-center HR-row rounded-lg relative`}
+          >
+            <div className={``}>
+              <p className="text-base ">
+                من {vacations} الي {vacations}
+              </p>
+            </div>
+            <div className={``}>
+              <p className="text-base ">سنويا</p>
+            </div>
+          </div>
+          <div
+            className={`bg-[#F1F0F8] after:!bg-[#9586f5] flex gap-2 p-4 items-center HR-row rounded-lg relative`}
+          >
+            <div className={``}>
+              <p className="text-base ">
+                من {vacations} الي {vacations}
+              </p>
+            </div>
+            <div className={``}>
+              <p className="text-base ">سنويا</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <h3>المباشرات</h3>
+          {/* #F1F0F8 */}
+          <div
+            className={`bg-[#F1F0F8] after:!bg-[#9586f5] flex gap-2 p-4 items-center HR-row rounded-lg relative`}
+          >
+            <div className={``}>
+              <p className="text-base ">علي رأس العمل</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <h3>السلف</h3>
+          {/* #F1F0F8 */}
+          <div
+            className={`bg-[#F1F0F8] after:!bg-[#9586f5] flex gap-2 p-4 items-center HR-row rounded-lg relative`}
+          >
+            <div className={``}>
+              <p className="text-base ">1500 ريال</p>
+            </div>
+            <div className={``}>
+              <p className="text-base ">{vacations}</p>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
