e57c912e01b812fa2e599983b89598f6e1e97804
yousefn <jon772002@gmail.com>
2024-09-19 08:17:29 +0300

    record

--- src/components/popups/Popups.jsx
+++ src/components/popups/Popups.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import AddIssueRecord from "./addIssueRecord/AddIssueRecord";
+
 import AddContractRecord from "./addContractRecord/AddContractRecord";
 import { resetPopups } from "@/globalState/Features/popupsSlice";
 import Attendance from "./attendance/Attendance";
