9d0ede7170993290830a788590638b1c4fe15606
yousefn <jon772002@gmail.com>
2024-09-27 00:08:31 +0300

    t

--- src/components/pages/home/appointments/appointment/Appointment.jsx
+++ src/components/pages/home/appointments/appointment/Appointment.jsx
@@ -8,13 +8,13 @@ const Appointment = () => {
   const isHijriDate = useSelector((state) => state?.formats?.isHijriDate);
   // const islamicDate = moment().format("iYYYY-iMM-iDD"); // Hijri Date
   return (
-    <li className="border border-gray-200 p-3 rounded items-center flex gap-3 font-medium">
+    <li className="border border-gray-200 p-3 rounded items-center flex flex-1 gap-3 font-medium">
       <div className="bg-[#048D5A] text-white px-4 py-2 rounded-2xl h-fit text-center">
         <h4 className="text-3xl font-semibold ">{dateNow().split("-")[2]}</h4>
         <p>{monthName(null, +(dateNow().split("-")[1]))}</p>
         <p>{dateNow().split("-")[0]}</p>
       </div>
-      <div className="flex flex-col p-3 gap-3">
+      <div className="flex flex-1 flex-col p-3 gap-3">
         <div className="flex justify-between items-center gap-2">
           <h5>جلسة مرافعة</h5>
           <span className="text-gray-400 text-sm">12:00 صباحا</span>
