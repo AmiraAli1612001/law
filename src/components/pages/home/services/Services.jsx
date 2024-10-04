e08b1e09db8658e13d0ed8c9d72a85277a837e4a
yousefn <jonabilmc@gmail.com>
2024-10-04 15:44:36 +0300

    t

--- src/components/pages/home/services/Services.jsx
+++ src/components/pages/home/services/Services.jsx
@@ -12,7 +12,7 @@ const Services = () => {
   const openStyles = "!bg-[#048D5A] !text-white ";
   const linkStyles = `font-medium p-2 bg-white rounded block hover:bg-[#048D5A] hover:text-white transition-all `;
   return (
-    <div className="bg-bgGray p-4 flex flex-col gap-2 h-full rounded-2xl border drop-shadow-sm">
+    <div className="bg-bgGray p-4 flex flex-col gap-6 h-full rounded-2xl border drop-shadow-sm">
       <div className="flex justify-between">
         <h3 className="text-lg font-semibold">أبرز الخدمات</h3>
         <nav className="flex gap-4">
@@ -36,7 +36,7 @@ const Services = () => {
           </button>
         </nav>
       </div>
-      <ul className="grid grid-cols-2 gap-4 flex-1 overflow-y-auto">
+      <ul className="grid grid-cols-2 gap-4 flex-1 overflow-y-auto auto-rows-max">
         <Service
           link={"/issues"}
           title={"اصدار قضية"}
