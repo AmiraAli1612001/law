360cc39d3fed3b19e9240298a840f7d6fcbf6c36
jo7722 <jonabilmc@gmail.com>
2024-10-31 17:13:29 +0300

    contract payments

--- src/components/shared/payment/paymentSelector/PaymentSelector.jsx
+++ src/components/shared/payment/paymentSelector/PaymentSelector.jsx
@@ -1,11 +1,111 @@
 import React, { useEffect, useRef, useState } from "react";
 import DynamicList from "../../dynamicList/DynamicList";
 import "./styles/styles.css";
+const Payment = () => {
+  const [paymentType, setPaymentType] = useState(0);
+  return (
+    <div
+      className={`${
+        paymentType == 0 ? "!grid-cols-3" : "!grid-cols-4"
+      } small-inputs `}
+    >
+      {/* payment payment attachment !*/}
+      <div className="simple-input">
+        <select
+          type="text"
+          name=""
+          id="contractValue"
+          // {...register("contractValue", {
+          //   required: "يجب ادخال مبلغ العقد",
+          // })}
+          placeholder="ادخل مبلغ العقد"
+          onChange={(e) => {
+            setPaymentType(e.target.value);
+          }}
+        >
+          <option value="0">بتاريخ محدد</option>
+          <option value="1">بصدور حكم</option>
+        </select>
+        {/* <p className="input-error">{errors.contractValue?.message}</p> */}
+      </div>
+      {/* value */}
+      {paymentType == 0 ? (
+        <>
+          <div className="simple-input">
+            <input
+              type="date"
+              defaultValue={new Date().toISOString().slice(0, 10)}
+              name=""
+              // {...register("contractValue", {
+              //   required: "يجب ادخال مبلغ العقد",
+              // })}
+            />
+            {/* <p className="input-error">{errors.contractValue?.message}</p> */}
+          </div>
+          <div className="simple-input">
+            <input
+              type="text"
+              name=""
+              id="contractValue"
+              // {...register("contractValue", {
+              //   required: "يجب ادخال مبلغ العقد",
+              // })}
+              placeholder="مبلغ الدفعة المستحقة"
+            />
+            {/* <p className="input-error">{errors.contractValue?.message}</p> */}
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="simple-input">
+            <input
+              type="date"
+              defaultValue={new Date().toISOString().slice(0, 10)}
+              name=""
+              // {...register("contractValue", {
+              //   required: "يجب ادخال مبلغ العقد",
+              // })}
+              placeholder="تاريخ صدور الحكم"
+            />
+            {/* <p className="input-error">{errors.contractValue?.message}</p> */}
+          </div>
+          {/* 7okm */}
+          <div className="simple-input">
+            <select
+              type="text"
+              name=""
+              // id="contractValue"
+              // {...register("contractValue", {
+              //   required: "يجب ادخال مبلغ العقد",
+              // })}
+            >
+              <option value="">اختر الحكم</option>
+              <option value="0">حكم 1</option>
+              <option value="1">حكم 2</option>
+            </select>
+          </div>
+          <div className="simple-input">
+            <input
+              type="text"
+              name=""
+              id="contractValue"
+              // {...register("contractValue", {
+              //   required: "يجب ادخال مبلغ العقد",
+              // })}
+              placeholder="مبلغ الدفعة المستحقة"
+            />
+            {/* <p className="input-error">{errors.contractValue?.message}</p> */}
+          </div>
+          
+        </>
+      )}
+    </div>
+  );
+};
 const PaymentSelector = () => {
   const [activeSection, setActiveSection] = useState(0);
   const [payType, setPayType] = useState(0);
   const [middlemanPayType, setMiddlemanPayType] = useState(0);
-  const [paymentType, setPaymentType] = useState(1);
   function handleClick(payType) {
     setPayType(payType);
   }
@@ -92,9 +192,7 @@ const PaymentSelector = () => {
             </div>
             {/* date */}
             <div className="simple-input">
-            <label htmlFor="">
-                تاريخ الدفع
-              </label>
+              <label htmlFor="">تاريخ الدفع</label>
               <input
                 type="date"
                 defaultValue={new Date()}
@@ -112,139 +210,9 @@ const PaymentSelector = () => {
         <section
           className={`${payType == 1 && "active"} payments h-full bg-bgGreen  `}
         >
-          <div className="small-inputs">
-            {/* contract value !*/}
-            <div className="simple-input">
-              {/* <label htmlFor="">مبلغ العقد</label>
-              <input
-                type="text"
-                name=""
-                id="contractValue"
-                // {...register("contractValue", {
-                //   required: "يجب ادخال مبلغ العقد",
-                // })}
-                placeholder="ادخل مبلغ العقد"
-              /> */}
-              {/* <p className="input-error">{errors.contractValue?.message}</p> */}
-            </div>
-            {/* middleman percentage !*/}
-            <div className="simple-input">
-              {/* <label htmlFor="">
-                {middlemanPayType == 0 ? "نسبة الوسيط" : "قيمة الوسيط"}
-              </label> */}
-              {/* <div className=" relative flex">
-                <input
-                  type="text"
-                  name=""
-                  defaultValue={0}
-                  id="middlemanPercentage"
-                  //   {...register("middlemanPercentag", {
-                  //     required: "يجب ادخال مبلغ العقد",
-                  //   })}
-                  className="!rounded-e-none"
-                  placeholder=""
-                />
-                <select
-                  type="text"
-                  name=""
-                  id="middlemanPayType"
-                  className=" !w-20 !rounded-s-none"
-                  //   {...register("middlemanPayType", {
-                  //     required: "يجب كتابة عنوان العقد",
-                  //   })}
-                  placeholder=""
-                  onChange={(e) => {
-                    setMiddlemanPayType(e.target.value);
-                  }}
-                >
-                  <option value="0">نسبة</option>
-                  <option value="1">قيمة</option>
-                </select>
-              </div> */}
-              {/* <p className="input-error">
-                {errors.middlemanPercentage?.message}
-              </p> */}
-            </div>
-          </div>
-          {/* payments */}
           <div>
             <DynamicList title={"الدفعات"}>
-              <div className="small-inputs !grid-cols-3">
-                {/* payment attachment !*/}
-                <div className="simple-input">
-                  <select
-                    type="text"
-                    name=""
-                    id="contractValue"
-                    // {...register("contractValue", {
-                    //   required: "يجب ادخال مبلغ العقد",
-                    // })}
-                    placeholder="ادخل مبلغ العقد"
-                    onChange={(e) => {
-                      setPaymentType(e.target.value);
-                    }}
-                  >
-                    <option value="1">بتاريخ محدد</option>
-                    <option value="2">بصدور حكم</option>
-                  </select>
-                  {/* <p className="input-error">{errors.contractValue?.message}</p> */}
-                </div>
-                {/* value */}
-                {paymentType == 1 ? (
-                  <>
-                    <div className="simple-input">
-                      <input
-                        type="date"
-                        defaultValue={new Date().toISOString().slice(0, 10)}
-                        name=""
-                        // {...register("contractValue", {
-                        //   required: "يجب ادخال مبلغ العقد",
-                        // })}
-                      />
-                      {/* <p className="input-error">{errors.contractValue?.message}</p> */}
-                    </div>
-                    <div className="simple-input">
-                      <input
-                        type="text"
-                        name=""
-                        id="contractValue"
-                        // {...register("contractValue", {
-                        //   required: "يجب ادخال مبلغ العقد",
-                        // })}
-                        placeholder="مبلغ الدفعة المستحقة"
-                      />
-                      {/* <p className="input-error">{errors.contractValue?.message}</p> */}
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div className="simple-input">
-                      <input
-                        type="date"
-                        defaultValue={new Date()}
-                        name=""
-                        // {...register("contractValue", {
-                        //   required: "يجب ادخال مبلغ العقد",
-                        // })}
-                        placeholder="تاريخ صدور الحكم"
-                      />
-                      {/* <p className="input-error">{errors.contractValue?.message}</p> */}
-                    </div>
-                    <div className="simple-input">
-                      <input
-                        type="text"
-                        name=""
-                        id="contractValue"
-                        // {...register("contractValue", {
-                        //   required: "يجب ادخال مبلغ العقد",
-                        // })}
-                        placeholder="مبلغ الدفعة المستحقة"
-                      />
-                      {/* <p className="input-error">{errors.contractValue?.message}</p> */}
-                    </div>
-                  </>
-                )}
-              </div>
+              <Payment />
             </DynamicList>
           </div>
         </section>
